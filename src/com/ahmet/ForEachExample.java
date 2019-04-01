b3fad29bdf55c110b946fc1c3a5b3a6f5e02f257
amasyali <ahmetamasyali@hotmail.com>
2019-04-01 15:21:11 +0300

    map stream example

---
+++ src/com/ahmet/ForEachExample.java
@@ -0,0 +1,29 @@
+package com.ahmet;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ForEachExample
+{
+    public static void main(String[] args)
+    {
+        Person person1 = new Person("Burak");
+        Person person2 = new Person("Mehmet");
+        Person person3 = new Person("Ahmet");
+
+        List<Person> userList = Arrays.asList(person1, person2, person3);
+
+        Map<String,Integer> ages = new HashMap<String, Integer>();
+
+        ages.put("ahmet",27);
+        ages.put("mehmet",30);
+        ages.put("ali",12);
+
+        ages.forEach((name,age) -> System.out.println("Name: "+name + " Age: " + age));
+
+        userList.forEach(System.out::println);
+    }
+
+}
