4ce30b59ac7d07c5d3d38c864a50f8a40ec5cfc4
amasyali <ahmetamasyali@hotmail.com>
2020-07-14 12:41:56 +0300

    stream tests

--- src/com/ahmet/lambda/LambdaInterfaceExample.java
+++ src/com/ahmet/lambda/LambdaInterfaceExample.java
@@ -2,9 +2,29 @@ package com.ahmet.lambda;
 
 public class LambdaInterfaceExample
 {
+
+    public static double sum(double a, double b , Calculator calculate)
+    {
+        return calculate.calculate(a, b);
+    }
+
+    @FunctionalInterface
+    public interface Calculator
+    {
+        double calculate(double a, double b);
+    }
+
     public static void main(String[] args)
     {
-        double result1 = sum(5.1, 3.7, (a, b) -> a + b);
+        double result1 = sum(5.1, 3.7, new Calculator()
+        {
+            @Override
+            public double calculate(double a, double b)
+            {
+                return a + b;
+            }
+        });
+
         System.out.println(result1);
 
 
@@ -15,17 +35,10 @@ public class LambdaInterfaceExample
 
     }
 
-    public static double sum(double a, double b , Calculate calculate)
-    {
-        return calculate.calculate(a, b);
-    }
 
 
-    @FunctionalInterface
-    public interface Calculate
-    {
-        double calculate(double a, double b);
-    }
+
+
 
 
 }
